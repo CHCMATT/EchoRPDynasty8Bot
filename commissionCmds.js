41a5537b4721acd5f4335c4c7c406ae61f6ff3b4
Matthew Anthony <misterbaboo@gmail.com>
2023-04-17 17:33:09 -0400

    Add notification when commission is reset by bot

--- commissionCmds.js
+++ commissionCmds.js
@@ -31,4 +31,14 @@ module.exports.weeklyReport = async (client) => {
 		.setColor('EDC531');
 
 	await client.channels.cache.get(process.env.COMMISSION_REPORT_CHANNEL_ID).send({ embeds: [embed] });
+
+	// color palette: https://coolors.co/palette/706677-7bc950-fffbfe-13262b-1ca3c4-b80600-1ec276-ffa630
+	var now = Math.floor(new Date().getTime() / 1000.0);
+	var repDate = `<t:${now}:d>`
+	var reason = `Automatic Commission Report Triggered on ${repDate}`
+	var notificationEmbed = new EmbedBuilder()
+		.setTitle('Commission Modified Automatically:')
+		.setDescription(`All realtor's commissions have been reset to \`$0\`.\n\n**Reason:** ${reason}.`)
+		.setColor('#1EC276');
+	await client.channels.cache.get(process.env.COMMISSION_LOGS_CHANNEL_ID).send({ embeds: [notificationEmbed] });
 };
