ee79ad48c326c98c6c7c507f33afb014fd017d5f
Matthew Anthony <misterbaboo@gmail.com>
2023-04-28 00:33:35 -0400

    Remove init cmd

--- startup.js
+++ startup.js
@@ -7,8 +7,6 @@ module.exports.startUp = async (client) => {
 	var channel = await client.channels.fetch(process.env.EMBED_CHANNEL_ID);
 	var oldEmbed = await dbCmds.readMsgId("embedMsg");
 
-	await dbCmds.setFinanceNum("financeNum", 0)
-
 	try {
 		await channel.messages.fetch(oldEmbed);
 		editEmbed.editEmbed(client);
