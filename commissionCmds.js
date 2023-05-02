86d927e367cf254f0750ba36ae3ffacd7ba8792b
Matthew Anthony <misterbaboo@gmail.com>
2023-05-01 23:17:06 -0400

    Remove more unused vars

--- commissionCmds.js
+++ commissionCmds.js
@@ -44,11 +44,9 @@ module.exports.commissionReport = async (client) => {
 		await client.channels.cache.get(process.env.COMMISSION_LOGS_CHANNEL_ID).send({ embeds: [embed] });
 
 		// color palette: https://coolors.co/palette/706677-7bc950-fffbfe-13262b-1ca3c4-b80600-1ec276-ffa630
-		var now = Math.floor(new Date().getTime() / 1000.0);
-		var repDate = `<t:${now}:d>`
-		await dbCmds.setRepDate("lastCommissionReportDate", repDate);
+		await dbCmds.setRepDate("lastCommissionReportDate", today);
 
-		var reason = `Commission Report triggered on ${repDate}`
+		var reason = `Commission Report triggered on ${today}`
 		var notificationEmbed = new EmbedBuilder()
 			.setTitle('Commission Modified Automatically:')
 			.setDescription(`\`System\` reset all realtor's commissions to \`$0\`.\n\n**Reason:** ${reason}.`)
