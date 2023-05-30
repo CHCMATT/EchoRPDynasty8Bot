3731f84e3a24c0ae6a908c12cb21e0417c417340
Matthew Anthony <misterbaboo@gmail.com>
2023-05-29 20:11:33 -0400

    Fix financing repos

--- commands/financeRepo.js
+++ commands/financeRepo.js
@@ -1,7 +1,7 @@
-var dbCmds = require('../dbCmds.js');
-var { PermissionsBitField, EmbedBuilder } = require('discord.js');
+let dbCmds = require('../dbCmds.js');
+let { PermissionsBitField, EmbedBuilder } = require('discord.js');
 
-var formatter = new Intl.NumberFormat('en-US', {
+let formatter = new Intl.NumberFormat('en-US', {
 	style: 'currency',
 	currency: 'USD',
 	maximumFractionDigits: 0
@@ -20,54 +20,83 @@ module.exports = {
 	],
 	async execute(interaction) {
 		if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-			var financingNum = interaction.options.getString('financingnumber').toUpperCase();
+			let financingNum = interaction.options.getString('financingnumber').toUpperCase();
 
-			var now = Math.floor(new Date().getTime() / 1000.0);
-			var today = `<t:${now}:d>`
-			var countFound = 0;
+			let now = Math.floor(new Date().getTime() / 1000.0);
+			let today = `<t:${now}:d>`
+			let countFound = 0;
 
-			var channel = await interaction.client.channels.fetch(process.env.FINANCING_AGREEMENTS_CHANNEL_ID);
-			var messages = await channel.messages.fetch();
+			let channel = await interaction.client.channels.fetch(process.env.FINANCING_AGREEMENTS_CHANNEL_ID);
+			let messages = await channel.messages.fetch();
 
 			messages.forEach(async (message) => {
 				if (message.embeds[0]) {
-					var embedTitle = message.embeds[0].data.title;
+					let embedTitle = message.embeds[0].data.title;
 					if (embedTitle === 'A new Financing Agreement has been submitted!') {
-						var msgRealtor = message.embeds[0].data.fields[0].value;
-						var msgSaleDate = message.embeds[0].data.fields[1].value;
-						var msgPaymentDate = message.embeds[0].data.fields[2].value;
-						var msgNextPaymentDateString = message.embeds[0].data.fields[3].value;
-						var msgFinanceNum = message.embeds[0].data.fields[4].value;
-						var msgClientInfo = message.embeds[0].data.fields[5].value;
-						var msgClientEmail = message.embeds[0].data.fields[6].value;
-						var msgStreetAddress = message.embeds[0].data.fields[7].value;
-						var msgSalePrice = message.embeds[0].data.fields[8].value;
-						var msgDownPayment = message.embeds[0].data.fields[9].value;
-						var msgAmtOwed = message.embeds[0].data.fields[10].value;
-						var msgFinancingAgreement = message.embeds[0].data.fields[11].value;
+						let msgRealtor = message.embeds[0].data.fields[0].value;
+						let msgSaleDate = message.embeds[0].data.fields[1].value;
+						let msgPaymentDate = message.embeds[0].data.fields[2].value;
+						let msgNextPaymentDate = message.embeds[0].data.fields[3].value;
+						let msgFinanceNum = message.embeds[0].data.fields[4].value;
+						let msgClientName = message.embeds[0].data.fields[5].value;
+						let msgClientInfo = message.embeds[0].data.fields[6].value;
+						let msgClientContact = message.embeds[0].data.fields[7].value;
+						let msgStreetAddress = message.embeds[0].data.fields[8].value;
+						let msgSalePrice = message.embeds[0].data.fields[9].value;
+						let msgDownPayment = message.embeds[0].data.fields[10].value;
+						let msgAmtOwed = message.embeds[0].data.fields[11].value;
+						let msgFinancingAgreement = message.embeds[0].data.fields[12].value;
+
+						let amtOwed = msgAmtOwed.replaceAll('$', '').replaceAll(',', '')
+						let formattedAfterPaymentAmt = formatter.format(amtOwed);
 
 						if (msgFinanceNum == financingNum) {
 							countFound++;
-							var embeds = [new EmbedBuilder()
-								.setTitle('A Financing Agreement has been repossessed!')
-								.addFields(
-									{ name: `Realtor Name:`, value: `${msgRealtor}` },
-									{ name: `Sale Date:`, value: `${msgSaleDate}`, inline: true },
-									{ name: `Latest Payment:`, value: `${msgPaymentDate}`, inline: true },
-									{ name: `Next Payment Due:`, value: `${msgNextPaymentDateString}`, inline: true },
-									{ name: `Financing ID Number:`, value: `${msgFinanceNum}` },
-									{ name: `Client Info:`, value: `${msgClientInfo}`, inline: true },
-									{ name: `Client Contact:`, value: `${msgClientEmail}`, inline: true },
-									{ name: `Street Address:`, value: `${msgStreetAddress}` },
-									{ name: `Sale Price:`, value: `${msgSalePrice}`, inline: true },
-									{ name: `Down Payment:`, value: `${msgDownPayment}`, inline: true },
-									{ name: `Amount Owed:`, value: `${msgAmtOwed}`, inline: true },
-									{ name: `Financing Agreement:`, value: `${msgFinancingAgreement}` },
-									{ name: `Notes:`, value: `- Property Repossession completed on ${today}.` },
-								)
-								.setColor('706677')];
+							if (message.embeds[0].data.fields[13]) {
+								var msgNotes = message.embeds[0].data.fields[13].value;
 
-							await interaction.client.channels.cache.get(process.env.COMPLETED_FINANCING_CHANNEL_ID).send({ embeds: embeds });
+								let embeds = new EmbedBuilder()
+									.setTitle('A Financing Agreement has been repossessed!')
+									.addFields(
+										{ name: `Realtor Name:`, value: `${msgRealtor}` },
+										{ name: `Sale Date:`, value: `${msgSaleDate}`, inline: true },
+										{ name: `Latest Payment:`, value: `${msgPaymentDate}`, inline: true },
+										{ name: `Next Payment Due:`, value: `${msgNextPaymentDate}`, inline: true },
+										{ name: `Financing ID Number:`, value: `${msgFinanceNum}` },
+										{ name: `Client Name:`, value: `${msgClientName}`, inline: true },
+										{ name: `Client Info:`, value: `${msgClientInfo}`, inline: true },
+										{ name: `Client Contact:`, value: `${msgClientContact}`, inline: true },
+										{ name: `Street Address:`, value: `${msgStreetAddress}` },
+										{ name: `Sale Price:`, value: `${msgSalePrice}`, inline: true },
+										{ name: `Down Payment:`, value: `${msgDownPayment}`, inline: true },
+										{ name: `Amount Owed:`, value: `${formattedAfterPaymentAmt}`, inline: true },
+										{ name: `Financing Agreement:`, value: `${msgFinancingAgreement}` },
+										{ name: `Notes:`, value: `${msgNotes}\n- Property Repossession completed on ${today}.` }
+									)
+									.setColor('706677');
+								await interaction.client.channels.cache.get(process.env.COMPLETED_FINANCING_CHANNEL_ID).send({ embeds: [embeds] });
+							} else {
+								let embeds = new EmbedBuilder()
+									.setTitle('A Financing Agreement has been repossessed!')
+									.addFields(
+										{ name: `Realtor Name:`, value: `${msgRealtor}` },
+										{ name: `Sale Date:`, value: `${msgSaleDate}`, inline: true },
+										{ name: `Latest Payment:`, value: `${msgPaymentDate}`, inline: true },
+										{ name: `Next Payment Due:`, value: `${msgNextPaymentDate}`, inline: true },
+										{ name: `Financing ID Number:`, value: `${msgFinanceNum}` },
+										{ name: `Client Name:`, value: `${msgClientName}`, inline: true },
+										{ name: `Client Info:`, value: `${msgClientInfo}`, inline: true },
+										{ name: `Client Contact:`, value: `${msgClientContact}`, inline: true },
+										{ name: `Street Address:`, value: `${msgStreetAddress}` },
+										{ name: `Sale Price:`, value: `${msgSalePrice}`, inline: true },
+										{ name: `Down Payment:`, value: `${msgDownPayment}`, inline: true },
+										{ name: `Amount Owed:`, value: `${formattedAfterPaymentAmt}`, inline: true },
+										{ name: `Financing Agreement:`, value: `${msgFinancingAgreement}` },
+										{ name: `Notes:`, value: `- Property Repossession completed on ${today}.` }
+									)
+									.setColor('706677');
+								await interaction.client.channels.cache.get(process.env.COMPLETED_FINANCING_CHANNEL_ID).send({ embeds: [embeds] });
+							}
 
 							await message.delete();
 
