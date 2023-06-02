a45173db01705f045ed899ccdd6d0109f14ff6f8
Matthew Anthony <misterbaboo@gmail.com>
2023-06-02 02:16:23 -0400

    Allow users to be created without nickname

--- personnelCmds.js
+++ personnelCmds.js
@@ -4,6 +4,11 @@ let dbCmds = require('./dbCmds.js');
 module.exports.initPersonnel = async (client, userId) => {
 	let guild = await client.guilds.fetch(process.env.DISCORD_SERVER_ID);
 	let user = await guild.members.fetch(userId);
-	let initCharName = user.nickname;
+	var initCharName;
+	if (user.nickname) {
+		initCharName = user.nickname;
+	} else {
+		initCharName = user.user.username;
+	}
 	await dbCmds.initPersStats(userId, initCharName);
 };
