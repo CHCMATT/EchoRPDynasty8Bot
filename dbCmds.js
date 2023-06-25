c54ae544a8262865ee92d7d9423b69abc4e8090e
Matthew Anthony <misterbaboo@gmail.com>
2023-06-25 15:49:21 -0400

    Remove unused db commands

--- dbCmds.js
+++ dbCmds.js
@@ -27,14 +27,9 @@ module.exports.subtractValueSumm = async (summaryName, value) => {
 	await d8SummaryInfo.findOneAndUpdate({ summaryName: summaryName }, { $inc: { value: -value } }, { upsert: true });
 };
 
-module.exports.setSummValue = async (summaryName, newValue) => {
-	await d8SummaryInfo.findOneAndUpdate({ summaryName: summaryName }, { value: newValue }, { upsert: true });
-};
-
 module.exports.resetSummValue = async (summaryName) => {
 	await d8SummaryInfo.findOneAndUpdate({ summaryName: summaryName }, { value: 0 }, { upsert: true });
 };
-
 
 
 // for finding and adding to the personnel's statistics
@@ -42,14 +37,11 @@ module.exports.initPersStats = async (discordId, discordNickname) => {
 	await d8PersonnelInfo.findOneAndUpdate({ discordId: discordId }, { discordId: discordId, charName: discordNickname, housesSold: 0, warehousesSold: 0, propertiesRepod: 0, propertiesQuoted: 0, activityChecks: 0, miscSales: 0, financialAgreements: 0, financialPayments: 0, currentCommission: 0, monthlyHousesSold: 0, monthlyWarehousesSold: 0, monthlyPropertiesRepod: 0, monthlyPropertiesQuoted: 0, monthlyActivityChecks: 0, monthlyMiscSales: 0, monthlyFinancialAgreements: 0, monthlyFinancialPayments: 0 }, { upsert: true });
 };
 
-module.exports.resetPersStats = async (discordId) => {
-	await d8PersonnelInfo.findOneAndUpdate({ discordId: discordId }, { housesSold: 0, warehousesSold: 0, propertiesRepod: 0, propertiesQuoted: 0, activityChecks: 0, financialAgreements: 0, financialPayments: 0, currentCommission: 0 }, { upsert: true });
-};
-
 module.exports.readPersStats = async (discordId) => {
 	let result = await d8PersonnelInfo.findOne({ discordId: discordId }, { discordId: 1, charName: 1, housesSold: 1, embedMsgId: 1, embedColor: 1, warehousesSold: 1, propertiesRepod: 1, propertiesQuoted: 1, activityChecks: 1, miscSales: 1, financialAgreements: 1, financialPayments: 1, currentCommission: 1, bankAccount: 1, _id: 0 });
 	return result;
 };
+
 
 module.exports.setPersColor = async (discordId, embedColor) => {
 	await d8PersonnelInfo.findOneAndUpdate({ discordId: discordId }, { embedColor: embedColor }, { upsert: true });
@@ -72,7 +64,6 @@ module.exports.setCharName = async (discordId, charName) => {
 };
 
 
-
 // monthly statistics report stuff
 module.exports.monthlyRealtorStatsRep = async () => {
 	let result = await d8PersonnelInfo.find({ charName: { $ne: null } }, { discordId: 1, charName: 1, monthlyHousesSold: 1, monthlyWarehousesSold: 1, monthlyPropertiesRepod: 1, monthlyPropertiesQuoted: 1, monthlyActivityChecks: 1, monthlyMiscSales: 1, monthlyFinancialAgreements: 1, monthlyFinancialPayments: 1, _id: 0 });
@@ -82,19 +73,6 @@ module.exports.monthlyRealtorStatsRep = async () => {
 module.exports.resetMonthlyRealtorStats = async (discordId) => {
 	await d8PersonnelInfo.findOneAndUpdate({ discordId: discordId }, { monthlyHousesSold: 0, monthlyWarehousesSold: 0, monthlyPropertiesRepod: 0, monthlyPropertiesQuoted: 0, monthlyActivityChecks: 0, monthlyMiscSales: 0, monthlyFinancialAgreements: 0, monthlyFinancialPayments: 0 }, { upsert: true });
 };
-
-
-
-// personnel message id stuff
-module.exports.setPersonnelMsgId = async (discordId, embedId) => {
-	await d8PersonnelInfo.findOneAndUpdate({ discordId: discordId }, { embedMsgId: embedId }, { upsert: true });
-};
-
-module.exports.readPersonnelMsgId = async (discordId) => {
-	let result = await d8PersonnelInfo.findOne({ discordId: discordId }, { embedMsgId: 1, _id: 0 });
-	return result.embedMsgId;
-};
-
 
 
 // commission stuff
@@ -121,7 +99,6 @@ module.exports.commissionRep = async () => {
 };
 
 
-
 // for setting message ID of current Discord embed message
 module.exports.setMsgId = async (summaryName, newValue) => {
 	await d8SummaryInfo.findOneAndUpdate({ summaryName: summaryName }, { msgId: newValue }, { upsert: true });
@@ -136,7 +113,6 @@ module.exports.readMsgId = async (summaryName) => {
 		return `Value not found for ${summaryName}.`;
 	}
 };
-
 
 
 // for setting string of latest commission report date
@@ -168,7 +144,6 @@ module.exports.readFinanceNum = async (summaryName) => {
 		return `Value not found for ${summaryName}`;
 	}
 };
-
 
 
 module.exports.currStats = async () => {
