4e72e0f579bd73b17053343dc6ec2be18c821b8b
Matthew Anthony <misterbaboo@gmail.com>
2023-10-30 22:26:51 -0400

    Add checkmypay command

---
+++ commands/checkMyCommission.js
@@ -0,0 +1,64 @@
+let moment = require('moment');
+let dbCmds = require('../dbCmds.js');
+let personnelCmds = require('../personnelCmds.js');
+let { PermissionsBitField, EmbedBuilder } = require('discord.js');
+
+let formatter = new Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'USD',
+	maximumFractionDigits: 0
+});
+
+module.exports = {
+	name: 'checkmypay',
+	description: 'Displays your current commission and pay information',
+	async execute(interaction) {
+		try {
+			if (interaction.member._roles.includes(process.env.FULL_TIME_ROLE_ID) || interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+
+				var personnelStats = await dbCmds.readPersStats(interaction.member.user.id);
+
+				let currentCommission = 0;
+				let monthlyCommission = 0;
+				let currentMiscPay = 0;
+				let bankAccount = 'n/a';
+
+				if (!Object.is(personnelStats, null)) {
+					currentCommission = personnelStats.currentCommission
+					monthlyCommission = personnelStats.monthlyCommission
+					currentMiscPay = personnelStats.currentMiscPay
+					bankAccount = personnelStats.bankAccount
+				}
+
+				let formattedCurrCommission = formatter.format(currentCommission);
+				let formattedMonthlyCommission = formatter.format(monthlyCommission);
+				let formattedCurrMiscPay = formatter.format(currentMiscPay);
+
+				await interaction.reply({ content: `Your current pay information:\n> Commission this pay period: \`${formattedCurrCommission}\`\n> Commission this month: \`${formattedMonthlyCommission}\`\n> Misc. pay this pay period: \`${formattedCurrMiscPay}\`\n> Your bank account number: \`${bankAccount}\``, ephemeral: true })
+
+			} else {
+				await interaction.reply({ content: `:x: You must have the \`Full-Time\` role or the \`Administrator\` permission to use this function.`, ephemeral: true });
+			}
+		} catch (error) {
+			if (process.env.BOT_NAME == 'test') {
+				console.error(error);
+			} else {
+				console.error(error);
+
+				let errTime = moment().format('MMMM Do YYYY, h:mm:ss a');;
+				let fileParts = __filename.split(/[\\/]/);
+				let fileName = fileParts[fileParts.length - 1];
+
+				console.log(`Error occured at ${errTime} at file ${fileName}!`);
+
+				let errorEmbed = [new EmbedBuilder()
+					.setTitle(`An error occured on the ${process.env.BOT_NAME} bot file ${fileName}!`)
+					.setDescription(`\`\`\`${error.toString().slice(0, 2000)}\`\`\``)
+					.setColor('B80600')
+					.setFooter({ text: `${errTime}` })];
+
+				await interaction.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL_ID).send({ embeds: errorEmbed });
+			}
+		}
+	},
+};
--- dbCmds.js
+++ dbCmds.js
@@ -36,11 +36,41 @@ module.exports.resetSummValue = async (summaryName) => {
 
 // for finding and adding to the personnel's statistics
 module.exports.initPersStats = async (discordId, discordNickname) => {
-	await d8PersonnelInfo.findOneAndUpdate({ discordId: discordId }, { discordId: discordId, charName: discordNickname, housesSold: 0, warehousesSold: 0, propertiesRepod: 0, propertiesQuoted: 0, activityChecks: 0, miscSales: 0, financialAgreements: 0, currentCommission: 0, monthlyHousesSold: 0, monthlyWarehousesSold: 0, monthlyPropertiesRepod: 0, monthlyPropertiesQuoted: 0, monthlyActivityChecks: 0, monthlyMiscSales: 0, monthlyFinancialAgreements: 0, settingQuotePing: true, settingReimbursementPing: true, quotesReviewed: 0, monthlyQuotesReviewed: 0, monthlyContactRequests: 0, contactRequests: 0 }, { upsert: true });
+	await d8PersonnelInfo.findOneAndUpdate({ discordId: discordId }, {
+		// personnel data
+		discordId: discordId, charName: discordNickname,
+
+		// overall data
+		housesSold: 0, warehousesSold: 0, propertiesRepod: 0, propertiesQuoted: 0, activityChecks: 0, miscSales: 0, financialAgreements: 0, quotesReviewed: 0, contactRequests: 0,
+
+		//pay data
+		currentCommission: 0, monthlyCommission: 0, currentMiscPay: 0, bankAccount: 'n/a',
+
+		// monthly data
+		monthlyHousesSold: 0, monthlyWarehousesSold: 0, monthlyPropertiesRepod: 0, monthlyPropertiesQuoted: 0, monthlyActivityChecks: 0, monthlyMiscSales: 0, monthlyFinancialAgreements: 0, monthlyQuotesReviewed: 0, monthlyContactRequests: 0,
+
+		// settings data
+		settingQuotePing: true, settingReimbursementPing: true, settingRepossessionPing: true
+
+	}, { upsert: true });
 };
 
 module.exports.readPersStats = async (discordId) => {
-	let result = await d8PersonnelInfo.findOne({ discordId: discordId }, { discordId: 1, charName: 1, housesSold: 1, embedMsgId: 1, embedColor: 1, warehousesSold: 1, propertiesRepod: 1, propertiesQuoted: 1, activityChecks: 1, miscSales: 1, financialAgreements: 1, quotesReviewed: 1, currentCommission: 1, bankAccount: 1, contactRequests: 1, _id: 0 });
+	let result = await d8PersonnelInfo.findOne({ discordId: discordId }, {
+		// personnel data
+		discordId: 1, charName: 1,
+
+		// overall data
+		housesSold: 1, warehousesSold: 1, propertiesRepod: 1, propertiesQuoted: 1, activityChecks: 1, miscSales: 1, financialAgreements: 1, quotesReviewed: 1, contactRequests: 1,
+
+		//pay data
+		currentCommission: 1, monthlyCommission: 1, currentMiscPay: 1, bankAccount: 1,
+
+		// monthly data
+		monthlyHousesSold: 1, monthlyWarehousesSold: 1, monthlyPropertiesRepod: 1, monthlyPropertiesQuoted: 1, monthlyActivityChecks: 1, monthlyMiscSales: 1, monthlyFinancialAgreements: 1, monthlyQuotesReviewed: 1, monthlyContactRequests: 1,
+
+		_id: 0
+	});
 	return result;
 };
 
@@ -112,6 +142,11 @@ module.exports.resetCurrPay = async (discordId) => {
 module.exports.readCurrentCommission = async (discordId) => {
 	let result = await d8PersonnelInfo.findOne({ discordId: discordId }, { currentCommission: 1, _id: 0 });
 	return result.currentCommission;
+};
+
+module.exports.readCurrentMiscPay = async (discordId) => {
+	let result = await d8PersonnelInfo.findOne({ discordId: discordId }, { currentMiscPay: 1, _id: 0 });
+	return result.currentMiscPay;
 };
 
 module.exports.readCurrentMiscPay = async (discordId) => {
